293eb1e31dde0c7f9a109cfd38f109bb7da13efd
leoswaldo <lmaciasm10@gmail.com>
2014-12-13 13:20:27 -0600

    Adding utilities script, and parsing code in methods :)

--- table_content_generator.py
+++ table_content_generator.py
@@ -1,51 +1,75 @@
 #!/python3/bin/python3
 
-import subprocess
 from prettytable import PrettyTable
+import utilities
+
 
 class TableContentGenerator():
-    def generate_table(command):
-        # call the command and catch the output in using the comminicate() 
-        # method for Popen 
-        process = subprocess.Popen(command,
-                        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        stdout, stderr = process.communicate()
-        stdout_list = str(stdout).split('\\n')
-        
-        # Remove the headers from the output, it will always be the first row 
-        headers = stdout_list.pop(0)
-        # Createh the list using split(), since the rows are strings 
-        headers_list = str(headers).split()
-        headers_len = len(headers_list)
-        
-        # Create the table as PrettyTable to use its features 
-        content_table = PrettyTable(headers_list)
-        # One space between column edges and contents (default)
-        content_table.padding_width = 1
-        
-        # we need to make sure every row in the stdout of the command run has the
-        # right format, we iterate though all of them, in case they don't we fix it
-        # and the add it to the table 
-        for row in stdout_list:
-            row_fields = str(row).split()
-            row_len = len(row_fields)
-        
-            # we need to make sure the list has appropiate fields, so the list
-            # can be added to the prettytable  
-            if(row_len > headers_len):
-                # if the list has more fields we remove all of them otu of the limits
-                # and add them to the last field
-                row_fields[headers_len - 1:] = ['\n '.join(row_fields[headers_len - 1:]
-                    )]
-            elif(row_len < headers_len):
-                # if the list has less fields thant the headers we need to add fields
-                # to it, we add None 
-                row_fields.extend([None] * (headers_len - row_len))
-        
-            # add the the row to the table, once it has been fixed
-            content_table.add_row(row_fields)
-    
-        return content_table
 
-table = generate_table(['ps', 'aux', '--sort', '-rss'])
-print(table)
+    ## Method: __init__
+    #  Description: initialize the class vars
+    def __init__(self):
+        self.content_table = None
+
+    ## Method: generate_table
+    #  Description: generate content for the table
+    #  Parameters: command
+    #    command: command to run in the os to get the content of the table
+    def generate_table(self, command):
+        status, stdout, stderr = utilities.run_command(command)
+        # if success running the command, otherwise else
+        if(status == 0):
+            # the rows are separated by  '\n' so we need to split it
+            command_output = str(stdout).split('\\n')
+            # Remove the headers from the output, it will always be the
+            # first row
+            headers = command_output.pop(0)
+            # Createh the list using split(), since the rows are strings
+            header_fields = str(headers).split()
+            num_fields = len(header_fields)
+
+            # initialize the table as PrettyTable to use its features with
+            # headers from output, and add content
+            self.content_table = PrettyTable(header_fields)
+            self.add_content(command_output, num_fields)
+        else:
+            self.content_table = stdout
+
+        return self.content_table
+
+    ## Method: add_content
+    #  Description: iterate through command output and add it to the table with
+    #      right format
+    #  Parameters: command_output, num_fields
+    #      command_output: the result of running the command in the os
+    #      num_fields: the ammount of fields in the tables base on the headers
+    def add_content(self, command_output, num_fields):
+        # we need to make sure every row in the stdout of the command run has
+        # the right format, we iterate though all of them, in case they don't
+        # we fix it and then add it to the table
+        for output_row in command_output:
+            row_fields = str(output_row).split()
+            row_len = len(row_fields)
+
+            # we need to make sure the list has appropiate fields, so the list
+            # can be added to the prettytable
+            if(row_len > num_fields):
+                # if the list has more fields we remove all of them otu of the
+                # limits and add them to the last field
+                row_fields[num_fields - 1:] =\
+                    ['\n '.join(row_fields[num_fields - 1:])]
+            elif(row_len < num_fields):
+                # if the list has less fields thant the headers we need to
+                # add fields to it, we add None
+                row_fields.extend([None] * (num_fields - row_len))
+
+            # add the the row to the table, once it has been fixed
+            self.content_table.add_row(row_fields)
+
+if __name__ == '__main__':
+    print('This class script should not be called as main script')
+    '''
+    table = TableContentGenerator()
+    mytable = table.generate_table(['ps', 'aux', '--sort', '-rss'])
+    print(mytable)
+    '''
